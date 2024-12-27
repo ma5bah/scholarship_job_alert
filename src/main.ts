e2bfe2c9b593765b9cf506fa7de04bb267ed9120
Abu Md. Masbah Uddin <masbahuddin60@gmail.com>
2024-12-27 09:32:16 +0600

    added cronjob again

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,9 @@
-// let count = 0;
-// const main = async () => {
-//     console.log('Sending message to chat...');
-//     const {ok, message} = await send_message_to_chat(process.env.PERSONAL_CHAT_ID!, `Hello world! ${count++}`);
-//     if (ok) {
-//         console.log('Message sent:', message);
-//     } else {
-//         console.error('Error sending message:', message);
-//     }
-// }
-// new CronJob(
-//     // "0 4-10,12-21/2 * * *", // cronTime
-//     "* * * * *", // cronTime
-//     async function () {
-//         await main();
-//     }, // onTick
-//     null, // onComplete
-//     true, // start
-//     'Asia/Dhaka' // timeZone
-// );
-
 
 import MofaScholarshipService from "./scholarship/mofa.gov.bd";
 import {ShedScholarshipService} from "./scholarship/shed.gov.bd";
 import {send_message_to_chat} from "./services/telegram/bot";
 import {FirebaseAdminService} from "./services/firebase/firebase_admin/firebase_admin.service";
+import {CronJob} from "cron";
 const firebase_service=new FirebaseAdminService();
 const services = [
     new MofaScholarshipService(firebase_service),
@@ -34,9 +14,14 @@ const main = async () => {
     for (const service of services) {
         try {
             const new_data = await service.requestNewData();
-            for (const data of new_data) {
-                const messageContent = `New Scholarship is Available in ${service.get_name()}! Please check the link: ${service.get_url()}`;
-                const {ok, message} = await send_message_to_chat("@scholarship_machine", messageContent);
+
+
+            if (new_data.length > 0) {
+                const messageContent = `🚀 Exciting News! A new scholarship opportunity is now available on ${service.get_name()}! \n🌟 Don't miss out—check it out here: ${service.get_url()} 🎓`;
+
+                // const {ok, message} = await send_message_to_chat("@scholarship_machine", messageContent);
+                const {ok, message} = await send_message_to_chat("@scholarship_machine_official", messageContent);
+
 
                 if (!ok) {
                     console.error(`Error sending message for ${service.constructor.name}:`, message);
@@ -52,7 +37,15 @@ const main = async () => {
     }
 };
 
-main();
-
+new CronJob(
+    // "0 4-10,12-21/2 * * *", // cronTime
+    "*/10 * * * *", // cronTime
+    async function () {
+        await main();
+    }, // onTick
+    null, // onComplete
+    true, // start
+    'Asia/Dhaka' // timeZone
+);
 
 
